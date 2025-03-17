86eda2f0b61b9a65378dfc645283323f27e43de3
Kayne <158179241+SyphonAce@users.noreply.github.com>
2025-03-17 13:06:48 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import pyautogui, time
+
+# Data used to fill out the email
+emailData = [{}]
+
+
+for person in emailData:
+    # Allows user to kill script for 5 seconds
+    print(">>>>>>5 SECOND PAUSE PRESS CRTL + C <<<<<")
+    time.sleep(5)
